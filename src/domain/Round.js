50bf1fcd98652b245bdd501f347d05d10b757244
yuraup <o721@naver.com>
2025-10-24 14:53:18 +0900

    feat: 단일 라운드 구현 중 난수 선택

--- src/domain/Round.js
+++ src/domain/Round.js
@@ -1,5 +1,13 @@
+import { generateRandomNumber } from '../utils/randomNumber.js';
+
 export default class Round {
   constructor(cars) {
     this.cars = cars;
+  }
+
+  run() {
+    this.cars.forEach((car) => {
+      const randomNumber = generateRandomNumber();
+    });
   }
 }
