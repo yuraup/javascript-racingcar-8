b7054a2093c60c565c4c82873a52eca295fbec49
yuraup <o721@naver.com>
2025-10-24 13:40:55 +0900

    feat: 자동차 객체 생성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Car from './domain/Car.js';
 import { scanCarNames, scanTryCounts } from './utils/io.js';
 import { parseCarNames, parseTryCounts } from './utils/parse.js';
 import { validateTryCounts } from './utils/validate.js';
@@ -10,6 +11,8 @@ class App {
     const tryCountsInput = await scanTryCounts();
     const parsedTryCounts = parseTryCounts(tryCountsInput);
     validateTryCounts(parsedTryCounts);
+
+    const cars = parsedCarNames.map((car) => new Car(car));
   }
 }
 
