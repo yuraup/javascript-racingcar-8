b5db5c203d368cc585d1d4cc0f5bc787233e2002
yuraup <o721@naver.com>
2025-10-24 11:07:02 +0900

    feat: 시도 횟수 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { scanCarNames } from './utils/io.js';
+import { scanCarNames, scanTryCounts } from './utils/io.js';
 import { parseCarNames } from './utils/parse.js';
 
 class App {
   async run() {
     const carNamesInput = await scanCarNames();
     const parsedCarNames = parseCarNames(carNamesInput);
+
+    const tryCountsInput = await scanTryCounts();
   }
 }
 
--- src/utils/io.js
+++ src/utils/io.js
@@ -4,3 +4,6 @@ export const scanCarNames = async () =>
   await Console.readLineAsync(
     '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   );
+
+export const scanTryCounts = async () =>
+  await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
