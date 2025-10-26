966c365b6882778d9095bf0ce09656cf5ec08fcb
yuraup <o721@naver.com>
2025-10-26 17:00:10 +0900

    test: 우승자 계산 로직 단위 테스트 추가

---
+++ __tests__/unitTest.js
@@ -0,0 +1,38 @@
+import { getWinnerNames } from '../src/utils/winner';
+import Car from '../src/domain/Car.js';
+
+describe('우승자 계산', () => {
+  test('가장 많이 이동한 자동차 반환', () => {
+    // given
+    const yura = new Car('yura');
+    const pobi = new Car('pobi');
+    const jun = new Car('jun');
+
+    yura.position = 5;
+    pobi.position = 3;
+    jun.position = 2;
+
+    // when
+    const winners = getWinnerNames([yura, pobi, jun]);
+
+    // then
+    expect(winners).toEqual(['yura']);
+  });
+
+  test('공동 우승자가 있을 경우 모두 반환', () => {
+    // given
+    const pobi = new Car('pobi');
+    const woni = new Car('woni');
+    const jun = new Car('jun');
+
+    pobi.position = 5;
+    woni.position = 5;
+    jun.position = 4;
+
+    // when
+    const winners = getWinnerNames([pobi, woni, jun]);
+
+    // then
+    expect(winners).toEqual(['pobi', 'woni']);
+  });
+});
